e258099df2e9e8d4bfadbb6e263d6317cd655c7b
fukunekolove <greenpoco29@gmail.com>
2024-10-04 13:50:12 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF=8">
+    <title>kadai_007</title>
+  </head>
+<body>
+  <?php
+$vegetable_date =['name' => 'onion', 'price' => '200', 'weight' => '160' ];
+
+print_r ($vegetable_date);
+
+?>
+
+</body>
+</html>
